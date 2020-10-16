9c5fed62194fa31c55fb5fed1681a2ab88555878
binhk1004 <binhk1004@naver.com>
2020-10-16 22:16:21 +0900

    [Toy Project] Finished URL Checker

--- src/github.com/binhk1004/main.go
+++ src/github.com/binhk1004/main.go
@@ -4,13 +4,17 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 )
 
+type requestResult struct {
+	url string
+	status string
+}
 var errRequestFailed = errors.New("요청이 실패 하였습니다.")
 
 func main()  {
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.co.kr/",
 		"https://www.google.co.kr/",
@@ -21,40 +25,23 @@ func main()  {
 		"https://www.instagram.com/",
 		"https://nomadcoders.co/",
 	}
-
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
-	for url, result := range results{
-		fmt.Println(url, result)
+	for i := 0; i < len(urls); i++{
+		result := <-c
+		results[result.url] = result.status
 	}
-	c := make(chan string)
-	people := [5]string{"bin", "jina", "jun", "wuzu", "nua"}
-	for _, person := range people{
-		go isSexy(person, c)
-	}
-	for i:=0;  i < len(people); i++ {
-		fmt.Println("waiting for", i)
-		fmt.Println(<-c)
+	for url, status := range results{
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("확인중: ",url)
+func hitURL(url string, c chan<- requestResult)  {
 	resp, err := http.Get(url)
+	status := "성공 했습니다."
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "실패 했습니다."
 	}
-	return nil
-}
-
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 10)
-	c <- person + " is sexy"
+	c <- requestResult{url: url, status: status}
 }
