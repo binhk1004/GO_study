d37292639a6d6ab0ad3aacf432059507e07bf775
binhk1004 <binhk1004@naver.com>
2020-10-17 21:36:03 +0900

    [Toy Project] Web Scrapper

--- src/github.com/binhk1004/main.go
+++ src/github.com/binhk1004/main.go
@@ -1,47 +1,54 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
 	"net/http"
+	"strconv"
 )
 
-type requestResult struct {
-	url string
-	status string
-}
-var errRequestFailed = errors.New("요청이 실패 하였습니다.")
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main()  {
-	results := make(map[string]string)
-	c := make(chan requestResult)
-	urls := []string{
-		"https://www.airbnb.co.kr/",
-		"https://www.google.co.kr/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://nomadcoders.co/",
-	}
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-	for i := 0; i < len(urls); i++{
-		result := <-c
-		results[result.url] = result.status
-	}
-	for url, status := range results{
-		fmt.Println(url, status)
+	totalPages := getPages()
+
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
 	}
 }
 
-func hitURL(url string, c chan<- requestResult)  {
-	resp, err := http.Get(url)
-	status := "성공 했습니다."
-	if err != nil || resp.StatusCode >= 400 {
-		status = "실패 했습니다."
+func getPage(page int)  {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("요청중입니다.", pageURL)
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection){
+		pages = s.Find("a").Length()
+	})
+
+	return pages
+}
+
+func checkErr(err error)  {
+	if err != nil {
+		log.Fatalln(err)
 	}
-	c <- requestResult{url: url, status: status}
+}
+
+func checkCode(res *http.Response)  {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
 }
